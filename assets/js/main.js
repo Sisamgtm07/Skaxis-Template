b35b4e247b9f883643dd6687d0e184054dad5c00
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-06-05 20:58:08 +0545

    Menu-Toggle completed

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function dropdownMenu(){
+    let menuToggle=document.getElementById("nav-list");
+    if(menuToggle.style.display==="none"){
+        menuToggle.style.display="block";
+    }
+    else{
+       menuToggle.style.display="none";
+    }
+}
